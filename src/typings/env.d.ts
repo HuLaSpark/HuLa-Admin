ee1e056779e8178b10afa25123aabf474644b208
nongyehong <2439646234@qq.com>
2023-12-16 15:45:44 +0800

    :rocket: chore(custom): 修改lint工具

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,4 +1,22 @@
 /**
+ * shims-vue.d.ts的作用
+ * 为了 typescript 做的适配定义文件，因为.vue 文件不是一个常规的文件类型，ts 是不能理解 vue 文件是干嘛的，
+ * 加这一段是是告诉 ts，vue 文件是这种类型的。
+ */
+declare module '*.vue' {
+  import { defineComponent } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
+}
+/*使用 window.$message 来调用 Message 组件的方法。由于 TypeScript 编译器无法识别全局变量 $message 的类型，
+我们可以使用 if (window.$message) 来进行判断，避免出现类型错误。*/
+declare interface Window {
+  $message: ReturnType<typeof useMessage>
+  $notification: ReturnType<typeof useNotification>
+  $loadingBar: ReturnType<typeof useLoadingBar>
+}
+
+/**
  *服务的环境类型
  * - dev: 开发环境
  * - test: 测试环境
